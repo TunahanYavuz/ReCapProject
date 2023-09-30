cc07c5e915ae0a93093cbfb9af653a3b3f7e37f0
Tunahan Yavuz <rey_tuna@hotmail.com>
2023-09-30 20:04:14 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car { CarName = "Audi A7", BrandID = 1, CarID = 1, ColorID = 2050, DailyPrice = 1800,Description="2016 Model Audi A7",ModelYear=(char)2016 },
+                new Car { CarName = "Audi A4", BrandID = 1, CarID = 2, ColorID = 1532, DailyPrice = 800,Description="2010 Model Audi A4",ModelYear=(char)2010 },
+                new Car { CarName = "Ford Mustang", BrandID = 2, CarID = 3, ColorID = 7854, DailyPrice = 2000,Description="2017 Model Ford Mustang",ModelYear=(char)2017 },
+                new Car { CarName = "Mercedes AMG", BrandID = 3, CarID = 4, ColorID = 6545, DailyPrice = 5000,Description="2022 Model Mercedes AMG",ModelYear=(char)2022 }
+            };
+        }
+
+        
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete  = _cars.SingleOrDefault(c=>c.CarID == car.CarID);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetBylD(int ID)
+        {
+            return _cars.Where(c => c.CarID == ID).ToList();
+        }
+
+        public List<Car> GetCars()
+        {
+            return _cars;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate=_cars.SingleOrDefault(c=>c.CarID==car.CarID);
+            _cars.
+        }
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Car
+    {
+        public string CarName { get; set; }
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+        public int ColorID { get; set; }
+        public string Description { get; set; }
+        public char ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+    }
+}
